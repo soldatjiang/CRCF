193ca77d63ec35637f710356e5b0b9352181f014
Jiang Shan <1516981776@qq.com>
2019-12-04 20:26:14 +0800

    ddd

--- implementation/detector_train.m
+++ implementation/detector_train.m
@@ -10,29 +10,27 @@ function g = detector_train(samplesf_large, yf_detector, num_training_samples, p
     %det_sz = size(yf_detector);
     T = numel(yf_detector);
     if num_training_samples<params.nSamples
-        samplesf_temp = samplesf_large(1:num_training_samples,:,:,:);
-        S_xx = sum(bsxfun(@times, prior_weights(1:num_training_samples), sum(conj(samplesf_temp) .* samplesf_temp, 4)), 1);
+        xf = samplesf_large(1:num_training_samples,:,:,:);
+        alpha = prior_weights(1:num_training_samples);
+        %S_xx = sum(bsxfun(@times, prior_weights(1:num_training_samples), sum(conj(samplesf_temp) .* samplesf_temp, 4)), 1);
     else
-        S_xx = sum(bsxfun(@times, prior_weights, sum(conj(samplesf_large) .* samplesf_large, 4)), 1);
+        %S_xx = sum(bsxfun(@times, prior_weights, sum(conj(samplesf_large) .* samplesf_large, 4)), 1);
+        xf = samplesf_large;
+        alpha = prior_weights;
     end
+    
+    xf_weighted = bsxfun(@times, alpha, xf);
+    xf_sum = sum(xf_weighted, 1);
+    
     admm_iterations = 2;
     
     while (i <= admm_iterations)
         %   solve for G- please refer to the paper for more details
-        B = S_xx + (T * mu);
-        if num_training_samples<params.nSamples
-            samplesf_temp = samplesf_large(1:num_training_samples,:,:,:);
-            S_lx = sum(sum(bsxfun(@times, prior_weights(1:num_training_samples), conj(samplesf_temp)), 1) .* l_f, 3);
-            S_hx = sum(sum(bsxfun(@times, prior_weights(1:num_training_samples), conj(samplesf_temp)), 1) .* h_f, 3);
+        if params.form2
+            error('Not Implemented!');
         else
-            S_lx = sum(sum(bsxfun(@times, prior_weights, conj(samplesf_large)), 1) .* l_f, 3);
-            S_hx = sum(sum(bsxfun(@times, prior_weights, conj(samplesf_large)), 1) .* h_f, 3);
+            
         end
-        %S_lx = sum(conj(model_xf) .* l_f, 3);
-        %S_hx = sum(conj(model_xf) .* h_f, 3);
-        g_f = (((1/(T*mu)) * bsxfun(@times, yf, model_xf)) - ((1/mu) * l_f)  + h_f) - ...
-            bsxfun(@rdivide,(((1/(T*mu)) * bsxfun(@times, model_xf, (S_xx .* yf))) ...
-            - ((1/mu) * bsxfun(@times, model_xf, S_lx)) + (bsxfun(@times, model_xf, S_hx))), B);
         
         %   solve for H
         h = (T/((mu*T)+ params.admm_lambda))  *   ifft2((mu*g_f) + l_f);    
